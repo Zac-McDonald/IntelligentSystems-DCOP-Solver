cba26c25825d4924f09fc0ecf90231b0cb1f29a3
thisismatthew <thisismatt@me.com>
2020-09-18 20:51:25 +1000

    + Testing some service interactions + ITimeService running

---
+++ src/main/java/service/testing/DecopUserAgent.java
@@ -0,0 +1,44 @@
+package service.testing;
+
+import jadex.base.PlatformConfiguration;
+import jadex.base.Starter;
+import jadex.bridge.service.IService;
+import jadex.commons.future.ISubscriptionIntermediateFuture;
+import jadex.micro.annotation.*;
+
+/**
+ *  Simple agent that uses globally available time services.
+ */
+@Agent
+@RequiredServices(
+        @RequiredService(name="timeservices",
+                type= ITimeService.class,
+                multiple=true,
+                binding=@Binding(scope=Binding.SCOPE_GLOBAL))
+)
+public class TimeUserAgent {
+    /**
+     *  The time services are searched and added at agent startup.
+     */
+    @AgentService
+    public void addTimeService(ITimeService timeservice) {
+        ISubscriptionIntermediateFuture<String> subscription = timeservice.subscribe();
+        while(subscription.hasNextIntermediateResult()) {
+            String  time    = subscription.getNextIntermediateResult();
+            String  platform    = ((IService)timeservice).getServiceIdentifier().getProviderId().getPlatformName();
+            System.out.println("New time received from "+platform+" at "+timeservice.getLocation()+": "+time);
+        }
+    }
+    /**
+     *  Start a Jadex platform and the TimeUserAgent.
+     */
+    public static void  main(String[] args) {
+        PlatformConfiguration config = PlatformConfiguration.getDefault();
+        config.setNetworkName("yourStudentNumber");
+        config.setNetworkPass("yourStudentNumber");
+        config.addComponent(TimeUserAgent.class);
+        config.setAwareness(true);
+        config.setGui(false);
+        Starter.createPlatform(config).get();
+    }
+}
---
+++ src/main/java/service/testing/StartOnSamePlatform.java
@@ -0,0 +1,21 @@
+package jadex.tutorials.week4.time;
+
+import jadex.base.PlatformConfiguration;
+import jadex.base.Starter;
+
+public class StartOnSamePlatform {
+    /**
+     * This program will start the TimeProvider agent and then two users on the
+     * same JadeX platform. You can start each agent type on their own platform
+     * using the main functions in each agent.
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        PlatformConfiguration config = PlatformConfiguration.getDefault();
+        config.setGui(true);
+        config.addComponent(TimeProviderAgent.class);
+        config.addComponent(TimeUserAgent.class);
+        config.addComponent(TimeUserAgent.class);
+        Starter.createPlatform(config).get();
+    }
+}
