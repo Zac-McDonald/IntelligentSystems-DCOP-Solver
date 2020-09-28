cc463759e61991f66056ad8b6813d367997aa8c8
thisismatthew <thisismatt@me.com>
2020-09-28 17:14:05 +1000

    + messageAll/Single/Group

--- src/main/java/dcop/solvers/ChatD1Agent.java
+++ src/main/java/dcop/solvers/ChatD1Agent.java
@@ -8,6 +8,8 @@ import jadex.bridge.service.component.IRequiredServicesFeature;
 import jadex.bridge.service.types.clock.IClockService;
 import jadex.commons.future.ITerminableIntermediateFuture;
 import jadex.micro.annotation.*;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -41,23 +43,66 @@ public class ChatD1Agent {
         ITerminableIntermediateFuture<IChatService> fut = requiredServicesFeature
                 .getRequiredServices("chatservices");
 
+        //make an event
         HashMap<String, Integer> map = new HashMap<String, Integer>();
         map.put("matthew", 1);
         map.put("zac", 2);
         map.put("Gus", 3);
         Event event = new Event("whisper",map, agent.getComponentIdentifier());
 
-        // need to edit that it doesn't send to every agent but instead to a range. all, some, or one
-//        fut.get()
-//                .forEach((it) -> // -- Java8 Lambda function usage, see: https://docs.oracle.com/javase/tutorial/java/javaOO/lambdaexpressions.html
-//                it.message(agent.getComponentIdentifier().getName(), event));
+        //message all agents
+//        messageAll(agent, fut, event);
 
+        //target is a reference to a specific agent.
+        //currently returning have dummy function for getting a target.
+        IInternalAccess target = getRandomTargetAgent(fut);
+        messageSingle(agent, fut, event, target);
+
+//        messageGroup(agent, fut, event, targets);
+
+    }
+
+    //TODO Replace this function with one that actually selects a target
+    public IInternalAccess getRandomTargetAgent(ITerminableIntermediateFuture<IChatService> fut){
+        Iterator<IChatService> it = fut.get().iterator();
+        IChatService i = it.next();
+        return i.getAgent();
+    }
+
+    public void messageAll(IInternalAccess agent, ITerminableIntermediateFuture<IChatService> fut, Event event){
         //the agents no longer send events to themselves
         Iterator<IChatService> it = fut.get().iterator();
         while(it.hasNext()){
             IChatService i = it.next();
             if (!i.getAgent().equals(agent)){
+                //as long as the agent doesnt match itself it sends the event
                 i.message(agent.getComponentIdentifier().getName(), event);
+            }
+        }
+    }
+
+    public void messageSingle(IInternalAccess agent, ITerminableIntermediateFuture<IChatService> fut, Event event, IInternalAccess target){
+        //the agents no longer send events to themselves
+        Iterator<IChatService> it = fut.get().iterator();
+        while (it.hasNext()){
+            IChatService i = it.next();
+            if (i.getAgent().equals(target)){
+                //loops through the futures and calls a message on the target
+                i.message(agent.getComponentIdentifier().getName(), event);
+            }
+        }
+    }
+
+    public void messageGroup(IInternalAccess agent, ITerminableIntermediateFuture<IChatService> fut, Event event, IInternalAccess[] targets){
+        //the agents no longer send events to themselves
+        Iterator<IChatService> it = fut.get().iterator();
+        while (it.hasNext()){
+            IChatService i = it.next();
+            for (IInternalAccess a :targets) {
+                if (i.getAgent().equals(a)){
+                    //loops through the futures and calls messages for the targets in the group
+                    i.message(agent.getComponentIdentifier().getName(), event);
+                }
             }
         }
     }
