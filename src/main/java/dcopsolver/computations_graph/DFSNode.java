dda9203d6f3cec38c638e2beac356be05159d0a2
Angus <angus.maude@hotmail.com>
2020-10-14 17:59:46 +1100

    updated get neighbours/children/parents functions

--- src/main/java/dcopsolver/computations_graph/DFSNode.java
+++ src/main/java/dcopsolver/computations_graph/DFSNode.java
@@ -53,42 +53,60 @@ public class DFSNode {
         this.adjacent = adjacent;
     }
 
-    public DFSNode GetParent(){
-        for (DFSEdge e: this.adjacent){
-            //finds parent node of highest node in set
-            if((e.child == this) && e.span){
-                return e.parent;
-            }
-        }
-        return null;
-    }
-
-    public List<DFSNode> GetChildren(boolean withPseudo){
+    public List<DFSNode> GetChildren(boolean Pseudo){
         List<DFSNode> list = new ArrayList<>();
         for (DFSEdge e : adjacent) {
-            if ((e.span || withPseudo) && (e.parent == this)) {
+            if ((e.span ^ Pseudo) && (e.parent == this)) {
                 list.add(e.child);
             }
         }
         return list;
     }
 
-    public List<DFSNode> GetParents(boolean withPseudo){
+    public List<DFSNode> GetAllChildren(){
         List<DFSNode> list = new ArrayList<>();
         for (DFSEdge e : adjacent) {
-            if ((e.span || withPseudo) && (e.child == this)) {
+            if (e.parent == this) {
+                list.add(e.child);
+            }
+        }
+        return list;
+    }
+
+    public List<DFSNode> GetParents(boolean Pseudo){
+        List<DFSNode> list = new ArrayList<>();
+        for (DFSEdge e : adjacent) {
+            if ((e.span ^ Pseudo) && (e.child == this)) {
                 list.add(e.parent);
             }
         }
         return list;
     }
 
-    public List<DFSNode> GetNeighbours(boolean withPseudo){
+    public List<DFSNode> GetAllParents(){
         List<DFSNode> list = new ArrayList<>();
         for (DFSEdge e : adjacent) {
-            if (e.span || withPseudo) {
+            if (e.child == this) {
+                list.add(e.parent);
+            }
+        }
+        return list;
+    }
+
+    public List<DFSNode> GetNeighbours(boolean Pseudo){
+        List<DFSNode> list = new ArrayList<>();
+        for (DFSEdge e : adjacent) {
+            if (e.span ^ Pseudo) {
                 list.add(e.EndNode(this));
             }
+        }
+        return list;
+    }
+
+    public List<DFSNode> GetAllNeighbours(){
+        List<DFSNode> list = new ArrayList<>();
+        for (DFSEdge e : adjacent) {
+            list.add(e.EndNode(this));
         }
         return list;
     }
